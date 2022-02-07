6147be13f515f6fb05bd9a591ffc8640b517cccb
froleod <82677399+froleod@users.noreply.github.com>
2022-02-07 22:39:19 +0300

    Add files via upload

--- forEst.js
+++ forEst.js
@@ -33,7 +33,7 @@ function getResult()
             {
                 if(TypeForest == "Хвойный") //формулы 1
                 {
-                    alert("Входные параметры: \n Пожар: низовой \n Класс ПО: II \n Лес: хвойный");
+                    // alert("Входные параметры: \n Пожар: низовой \n Класс ПО: II \n Лес: хвойный");
                     var valueFront = 0.0069*Math.pow(Vv,6)-0.1513*Math.pow(Vv,5)+1.0288*Math.pow(Vv,4)-2.2679*Math.pow(Vv,3)+3.9589*Math.pow(Vv,2)-2.6788*Vv+5;
                     var valueFlang = 0.0092*Math.pow(Vv,4)-0.2144*Math.pow(Vv,3)+1.5257*Math.pow(Vv,2)-1.0479*Vv+9;
                     var valueTyl = -0.0395*Math.pow(Vv,3)+0.5371*Math.pow(Vv,2)-0.7971*Vv+5;
@@ -42,7 +42,7 @@ function getResult()
                 }
                 else
                 {
-                    alert("Входные параметры: \n Пожар: низовой \n Класс ПО: II \n Лес: лиственный");
+                    // alert("Входные параметры: \n Пожар: низовой \n Класс ПО: II \n Лес: лиственный");
                     var valueFront = -0.0371*Math.pow(Vv,3)+ 0.4783*Math.pow(Vv,2) + 0.2457*Vv+15.5;
                     var valueFlang = -0.0059*Math.pow(Vv,5)+ 0.1459*Math.pow(Vv,4)- 1.2789*Math.pow(Vv,3)+4.413*Math.pow(Vv,2)- 2.9368*Vv+9;
                     var valueTyl = 0.0007*Math.pow(Vv,6)-0.0221*Math.pow(Vv,5)+0.2615*Math.pow(Vv,4)- 1.5121*Math.pow(Vv,3)+4.1847*Math.pow(Vv,2)-2.8724*Vv+4.5;
@@ -55,14 +55,14 @@ function getResult()
             {
                 if(TypeForest == "Хвойный") //формулы 3
                 {
-                    alert("Входные параметры: \n Пожар: низовой \n Класс ПО: III \n Лес: хвойный");
+                    // alert("Входные параметры: \n Пожар: низовой \n Класс ПО: III \n Лес: хвойный");
                     var valueFront = -0.0176*Math.pow(Vv,4)-0.3983*Math.pow(Vv,3)+7.9257*Math.pow(Vv,2)-4.6351*Vv+5;
                     var valueFlang = -0.0011*Math.pow(Vv,6)+0.0316*Math.pow(Vv,5)-0.3366*Math.pow(Vv,4)+1.5686*Math.pow(Vv,3)-2.6792*Math.pow(Vv,2)+1.5855*Vv+18;
                     var valueTyl = 0.0087*Math.pow(Vv,4)-0.2403*Math.pow(Vv,3)+1.9581*Math.pow(Vv,2)- 2.5641*Vv+5;
                 }
                 else
                 {
-                    alert("Входные параметры: \n Пожар: низовой \n Класс ПО: III \n Лес: лиственный" );
+                    // alert("Входные параметры: \n Пожар: низовой \n Класс ПО: III \n Лес: лиственный" );
                     var valueFront = -0.0107*Math.pow(Vv,5)+ 0.2633*Math.pow(Vv,4) - 2.5233*Math.pow(Vv,3) + 11.007*Math.pow(Vv,2) - 6.7781*Vv + 21;
                     var valueFlang = 0.0007*Math.pow(Vv,6) - 0.0269*Math.pow(Vv,5) + 0.3929*Math.pow(Vv,4) - 2.6682*Math.pow(Vv,3) + 8.0831*Math.pow(Vv,2) - 5.4897*Vv + 18;
                     var valueTyl = 0.0009*Math.pow(Vv,6) - 0.029*Math.pow(Vv,5) + 0.3582*Math.pow(Vv,4) - 2.1317*Math.pow(Vv,3) + 5.9064*Math.pow(Vv,2) - 4.1495*Vv + 10;
@@ -155,7 +155,7 @@ function getResult()
         resultVoda.innerHTML = "Расход воды на тушение, тонн: " + "<b>" + RashodVodi.toFixed(1) + "</b>" + "<br/> ";
         }
         else{
-            alert("1 класс ПО");
+            // alert("1 класс ПО");
             resultPTilte.innerHTML="ВЫХОДНЫЕ ПАРАМЕТРЫ";
             resultP.innerHTML="При I классе пожароопасности расчет распространения лесного пожара не производится";
             resultFront.innerHTML = "" ;
